7b5cbab183e947fc53897252beff2148ef8469a0
Dilmer Valecillos <dilmervst@gmail.com>
2019-03-18 06:03:33 -0600

    Added Debugger Example script

---
+++ Assets/Scripts/DebuggerExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebuggerExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("(" + gameObject.name + ") The Debugger Example is starting...");
+
+        // Debug.Log("(" + gameObject.name + ") is at position: " + gameObject.transform.position);
+
+        Debug.Log("(" + gameObject.name + ") is at position: " + gameObject.transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
