b8885cd3048af00d7ea5a99b72ed6fa22ef45e33
Dilmer Valecillos <dilmervst@gmail.com>
2019-04-11 14:24:03 -0600

    Added Tags and Layers session

---
+++ Assets/TagManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject playerGameObject = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log("playerGameObject gameObject: " + playerGameObject.gameObject.name);
+        Debug.Log("playerGameObject layer #: " + playerGameObject.layer);
+        Debug.Log("playerGameObject layer name: " + LayerMask.LayerToName(playerGameObject.layer));
+
+        GameObject enemyGameObject = GameObject.FindGameObjectWithTag("Enemy");
+        Debug.Log("enemyGameObject gameObject: " + enemyGameObject.gameObject.name);   
+        Debug.Log("enemyGameObject layer #: " + enemyGameObject.layer);
+        Debug.Log("enemyGameObject layer name: " + LayerMask.LayerToName(enemyGameObject.layer));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
